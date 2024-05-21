da3e7b48365e17014a2661576ba8f059d44f9ed0
NikEr2024 <n.erdyneev2000@gmail.com>
2024-05-22 01:19:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+number_of_completed_remote_sensing = 12
+чтоб_было_точь_в_точь = 10
+nimber_of_hours_spent = 1.5
+name_course = 'Python'
+time_na_odno_zadanie = nimber_of_hours_spent / number_of_completed_remote_sensing
+print(name_course,',' 'всего задач:', number_of_completed_remote_sensing,'затрачено часов:'
+      , nimber_of_hours_spent, 'среднее время выполнение ',
+      time_na_odno_zadanie)
+print()
+print(name_course,',' 'всего задач:', чтоб_было_точь_в_точь,'затрачено часов:'
+      , nimber_of_hours_spent, 'среднее время выполнение ',
+      time_na_odno_zadanie)
+
